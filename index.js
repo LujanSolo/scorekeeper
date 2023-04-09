4de9a790af5834a43367e4f5e793d689157414b0
LujanSolo <lujansolo@proton.me>
2023-04-09 13:49:49 -0700

    set initial values and began function building; html onclicks set to home and away teams

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+// set initial score values for home and away
+let homeScore = 0;
+let guestScore = 0;
+
+// set variables for homeScore and guestScore
+document.getElementById('home-el').textContent = homeScore;
+document.getElementById('guest-el').textContent = guestScore;
+
 // build functions for onclick events +1, +2, +3
+function addOneHome() {
+
+}
