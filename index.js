3c8b383fd0340521e62ab9461638d6fc50922da6
LujanSolo <lujansolo@proton.me>
2023-04-09 14:06:12 -0700

    corrected misspellings on html; added initial functions and default scores and text content

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-// set initial score values for home and away
+// set variables for homeScore and guestScore
+let homeScoreEl = document.getElementById('home-el');
+let guestScoreEl = document.getElementById('guest-el');
+
+homeScoreEl.textContent = 0;
+guestScoreEl.textContent = 0;
+
 let homeScore = 0;
 let guestScore = 0;
 
-// set variables for homeScore and guestScore
-document.getElementById('home-el').textContent = homeScore;
-document.getElementById('guest-el').textContent = guestScore;
 
 // build functions for onclick events +1, +2, +3
 function addOneHome() {
-
+  homeScore++;
+  homeScoreEl.textContent = homeScore;
 }
